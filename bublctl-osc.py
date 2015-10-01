182fcb7ca8cccdd5ae40fbceab1dc0477723bd94
Lehner Florian <dev@der-flo.net>
2015-10-01 21:23:02 +0200

    add some more features of the api to bublctl-osc
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- bublctl-osc.py
+++ bublctl-osc.py
@@ -20,9 +20,19 @@ class bublctlosc:
 		self.sid = (rep["results"]["sessionId"])
 		return
 
+	def update(self):
+		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
+		data = json.dumps({"name":"camera.updateSession", "parameters":{"sessionId":self.sid}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
+		req = requests.post(url, data=data, headers=self.header)
+		rep = req.json()
+		print rep
+		return
+
 	def stop(self):
 		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
 		data = json.dumps({"name":"camera.closeSession", "parameters":{"sessionId":self.sid}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
 		req = requests.post(url, data=data, headers=self.header)
 		rep = req.json()
 		print rep
@@ -31,6 +41,55 @@ class bublctlosc:
 	def takePicture(self):
 		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
 		data = json.dumps({"name":"camera.takePicture", "parameters":{"sessionId":self.sid}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
+		req = requests.post(url, data=data, headers=self.header)
+		rep = req.json()
+		print rep
+		return (rep["results"]["fileUri"])
+
+	def listPictures(self, count, size, thumbs):
+		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
+		data = json.dumps({"name":"camera.listImages", "parameters":{"entryCount":count, "maxSize":size, "includeThumb":bool(thumbs)}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
+		req = requests.post(url, data=data, headers=self.header)
+		rep = req.json()
+		print rep
+		return
+
+	def deletePicture(self, fileUri):
+		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
+		data = json.dumps({"name":"camera.delete", "parameters":{"fileUri":fileUri}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
+		req = requests.post(url, data=data, headers=self.header)
+		rep = req.json()
+		print rep
+		return
+
+	def getPicture(self, fileUri):
+		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
+		data = json.dumps({"name":"camera.delete", "parameters":{"fileUri":fileUri}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
+		req = requests.post(url, data=data, headers=self.header)
+		print req
+		return
+
+	def getPictureMetadata(self, fileUri):
+		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
+		data = json.dumps({"name":"camera.delete", "parameters":{"fileUri":fileUri}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
+		req = requests.post(url, data=data, headers=self.header)
+		rep = req.json()
+		print rep
+		return
+
+	def getOptions(self, optionlist=["captureMode", "iso", "shutterSpeed", "aperture",
+								"whiteBalance", "exposureCompensation", "fileFormat",
+								"exposureDelay", "sleepDelay", "offDelay", "totalSpace",
+								"remainingSpace", "gpsInfo", "hdr", "gyro",
+								"imageStabilization", "wifiPassword"]):
+		url = "http://" + self.ip + ":" + self.port +"/osc/commands/execute"
+		data = json.dumps({"name":"camera.getOptions", "parameters":{"sessionId":self.sid, "optionNames":optionlist}})
+		self.header["Content-Type"] = "application/json; charset=utf-8"
 		req = requests.post(url, data=data, headers=self.header)
 		rep = req.json()
 		print rep
@@ -57,5 +116,5 @@ if __name__ == "__main__":
 	con.state()
 	con.info()
 	con.start()
-	con.takePicture()
+	con.getOptions()
 	con.stop()
